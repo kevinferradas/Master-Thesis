9790e365b149d17fa5bac37ed81468d10e31052a
kevinferradas <142252179+kevinferradas@users.noreply.github.com>
2024-05-17 01:31:15 +0200

    Update contour_funcs.py

--- contour_funcs.py
+++ contour_funcs.py
@@ -28,8 +28,8 @@ class Gmatrix:
             self.particle_sign = -1
         self.particle_type = particle_type
         
-        self.GM = np.zeros((ntau, orb, orb), dtype=np.complex128) # shape=3?
-        self.GI = np.zeros((n, ntau, orb, orb), dtype=np.complex128)#shape=4?
+        self.GM = np.zeros((ntau, orb, orb), dtype=np.complex128) # shape=3
+        self.GI = np.zeros((n, ntau, orb, orb), dtype=np.complex128)#shape=4
         self.GR = np.zeros((n, n, orb, orb), dtype=np.complex128)
         self.GL = np.zeros((n, n, orb, orb), dtype=np.complex128)
         
@@ -40,7 +40,7 @@ class Gmatrix:
     def get_particle(self):
         return self.particle_type
     def init_mat(self):
-        self.GM = np.zeros_like(self.GM) # Return an array of zeros with the same shape and type as a given array.
+        self.GM = np.zeros_like(self.GM) # np.zeros_like()--> Return an array of zeros with the same shape and type as a given array.
 
 
     def init_lmx(self):
