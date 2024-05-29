65049bbdfc0b02fd3a0ecc7ada569451124669eb
kevinferradas <142252179+kevinferradas@users.noreply.github.com>
2024-05-29 14:38:12 +0200

    Update hubbard_lattice_kevin.py

--- hubbard_lattice_kevin.py
+++ hubbard_lattice_kevin.py
@@ -7,9 +7,9 @@ print("Imports done")
 
 
 
-fl = open("parameters")
-lines = fl.readlines()
-fl.close()
+fl = open("parameters") #The open() function opens a file, and returns it as a file object.
+lines = fl.readlines() # The readlines() method returns a list containing each line in the file as a list item.
+fl.close() # The close() method closes an open file.
 
 for line in lines:
     if line[:2] == "--":
@@ -62,7 +62,8 @@ Vloc[0,1,1,0] = -U/2
 # Intersite hoppings
 ###################################################################
 
-Hkin = np.array([-t*np.eye(2),-t*np.eye(2),-t*np.eye(2)], dtype=np.complex128)
+Hkin = np.array([-t*np.eye(2),-t*np.eye(2),-t*np.eye(2)], dtype=np.complex128) # Hkin.shape=(3,2,2)
+#np.eye(N) --> Return a 2-D, N X N  array (matrix) with ones on the diagonal and zeros elsewhere.
 
 
 
@@ -70,14 +71,14 @@ Hkin = np.array([-t*np.eye(2),-t*np.eye(2),-t*np.eye(2)], dtype=np.complex128)
 def generateGk(nk_points, n, ntau, states, particle_type=0):
     Gk = []
     for i in range(nk_points):
-        Gk.append(negf.Gmatrix(n, ntau, states, particle_type))
+        Gk.append(negf.Gmatrix(n, ntau, states, particle_type)) # The append() method appends an element to the end of the list.
     return Gk
 
 
 
 
 k_lattice = negf.Lattice(nkvec,nkvec,nkvec)
-Gk = generateGk(k_lattice.length, n, ntau, 2, 1)
+Gk = generateGk(k_lattice.length, n, ntau, 2, 1) # There are just 2 states
 Gloc = negf.Gmatrix(n, ntau, 2, 1)
 S = negf.Gmatrix(n, ntau, 2, 1) # Purely local
 
@@ -88,7 +89,7 @@ Hloc = np.zeros((2,2), dtype=np.complex128)
 mu = negf.matsubara_branch_init_hf_kspace(N, 0, k_lattice, Hloc, Hkin, Gk, Gloc, S, Vloc, interp, beta, 1)
 np.savetxt("Gloc_mat_up", Gloc.get_mat()[:,0,0]) # savetxt --> Save an array to a text file.
 np.savetxt("Gloc_mat_dn", Gloc.get_mat()[:,1,1])
-
+# Gloc.get_mat()[:,0,0]
 
 
 print("Work finished succesfully")
